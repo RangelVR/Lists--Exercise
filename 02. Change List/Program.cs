7f3cae8473bc29eb6a33296b2b8815b4d635e0fa
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-20 17:56:14 +0200

    Add files via upload

---
+++ 02. Change List/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string comand;
+
+            while ((comand = Console.ReadLine()) != "end")
+            {
+                string[] comandArray = comand.Split();
+                string operation = comandArray[0];
+
+                switch (operation)
+                {
+                    case "Delete":
+                        int deletedElement = int.Parse(comandArray[1]);
+                        numbers.Remove(deletedElement);
+                        break;
+
+                    case "Insert":
+                        int elemet = int.Parse(comandArray[1]);
+                        int position = int.Parse(comandArray[2]);
+                        numbers.Insert(position, elemet);
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
