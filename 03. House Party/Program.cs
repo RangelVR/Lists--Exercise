37f0c1cf6d60874d481464bf4e9eb86b464c8ab1
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-20 18:41:35 +0200

    Update Program.cs

--- 03. House Party/Program.cs
+++ 03. House Party/Program.cs
@@ -9,8 +9,8 @@ namespace _03.House_Party
         static void Main(string[] args)
         {
             List<string> guestList = new List<string>();
-            int commands = int.Parse(Console.ReadLine());   
-
+            int commands = int.Parse(Console.ReadLine());
+            
             for (int i = 0; i < commands; i++)
             {
                 string[] comm = Console.ReadLine().Split().ToArray();
@@ -18,21 +18,25 @@ namespace _03.House_Party
 
                 if (comm.Length == 3)
                 {
-                    if (guestList.Contains(name))
+                    if (!guestList.Contains(name))
+                    {
+                        guestList.Add(name);
+                    }
+                    else
                     {
                         Console.WriteLine($"{name} is already in the list!");
-                        continue;
                     }
-                    guestList.Add(name);
                 }
                 else if (comm.Length == 4)
                 {
                     if (guestList.Contains(name))
                     {
                         guestList.Remove(name);
-                        continue;
                     }
-                    Console.WriteLine($"{name} is not in the list!");
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
                 }
             }
             Console.WriteLine(string.Join(Environment.NewLine, guestList));
