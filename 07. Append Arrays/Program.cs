7ed1dc7e112ff96f661206c561695329903d0a23
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-23 22:14:34 +0200

    Add files via upload

---
+++ 07. Append Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListExc
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+
+
+            List<string> nums = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+            List<string> numbers = new List<string>();
+
+            foreach (var str in nums)
+            {
+                numbers.AddRange(str.Split( " ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToList());
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+    }
+
+}
