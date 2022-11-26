df6949eac14f8c5c49389fba9997feafd21d06a0
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-26 12:35:31 +0200

    Update Program.cs

--- 08. Anonymous Threat/Program.cs
+++ 08. Anonymous Threat/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,46 +8,41 @@ namespace _08._Anonymous_Threat
     {
         static void Main(string[] args)
         {
-            List<string> input = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .ToList();
-            string[] command = Console.ReadLine().Split().ToArray();
-            
-
+            List<string> input = Console.ReadLine()!.Split(' ').ToList();
+            string[] command = Console.ReadLine().Split();
             while (command[0] != "3:1")
             {
                 int startIndex = int.Parse(command[1]);
                 int endIndex = int.Parse(command[2]);
-
                 if (command[0] == "merge")
                 {
                     if (startIndex < 0) startIndex = 0;
                     if (endIndex >= input.Count) endIndex = input.Count - 1;
-
-                    for (int i = endIndex - startIndex; i > 0; i--)
+                    for (int diff = endIndex - startIndex; diff > 0; --diff)
                     {
                         input[startIndex] += input[startIndex + 1];
                         input.RemoveAt(startIndex + 1);
                     }
                 }
-                else if (command[0] == "divide")
+                if (command[0] == "divide")
                 {
-                    int partSize = input[startIndex].Length / int.Parse(command[2]);
-                    int counter = endIndex;
+                    int subParts = int.Parse(command[2]);
+                    int partSize = input[startIndex].Length / endIndex;
+                    int counter = subParts;
                     int off = 1;
-
-                    while (counter > 0)
+                    while (counter > 1)
                     {
                         counter--;
                         input.Insert(startIndex + off, new string(input[startIndex].Take(partSize).ToArray()));
                         off++;
                         input[startIndex] = new string(input[startIndex].Skip(partSize).ToArray());
                     }
+                    input.Insert(startIndex + off, input[startIndex]);
                     input.RemoveAt(startIndex);
                 }
-                command = Console.ReadLine().Split().ToArray();
+                command = Console.ReadLine().Split();
             }
-            Console.WriteLine(string.Join(" ", input));
+            Console.WriteLine(string.Join(' ', input));
         }
     }
 }
