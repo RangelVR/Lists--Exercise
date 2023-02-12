4c852d951a20b75ae427d9f7e0734c9d1649add4
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-12 19:41:02 +0200

    Update Program.cs

--- 03. House Party/Program.cs
+++ 03. House Party/Program.cs
@@ -1,45 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+int n = int.Parse(Console.ReadLine());
+List<string> guestList = new List<string>();
 
-namespace _03.House_Party
+while (n > 0)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> guestList = new List<string>();
-            int commands = int.Parse(Console.ReadLine());
-            
-            for (int i = 0; i < commands; i++)
-            {
-                string[] comm = Console.ReadLine().Split().ToArray();
-                string name = comm[0];
+    string command = Console.ReadLine();
+    string name = command.Split().First();
 
-                if (comm.Length == 3)
-                {
-                    if (!guestList.Contains(name))
-                    {
-                        guestList.Add(name);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{name} is already in the list!");
-                    }
-                }
-                else if (comm.Length == 4)
-                {
-                    if (guestList.Contains(name))
-                    {
-                        guestList.Remove(name);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{name} is not in the list!");
-                    }
-                }
-            }
-            Console.WriteLine(string.Join(Environment.NewLine, guestList));
+    if (command.Contains("not"))
+    {
+        if (guestList.Contains(name))
+        {
+            guestList.Remove(name);
+        }
+        else
+        {
+            Console.WriteLine($"{name} is not in the list!");
         }
     }
+    else
+    {
+        if (guestList.Contains(name))
+        {
+            Console.WriteLine($"{name} is already in the list!");
+            n--;
+            continue;
+        }
+
+        guestList.Add(name);
+    }
+
+    n--;
 }
+
+Console.WriteLine(string.Join(Environment.NewLine, guestList));
