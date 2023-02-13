e7cf991f27e8077265ad5b56c545677b045af7c3
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-14 00:55:36 +0200

    Update Program.cs

--- 08. Anonymous Threat/Program.cs
+++ 08. Anonymous Threat/Program.cs
@@ -1,48 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+List<string> input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
 
-namespace _08._Anonymous_Threat
+string[] command = Console.ReadLine().Split().ToArray();
+
+while (command[0] != "3:1")
 {
-    class Program
+
+    if (command[0] == "merge")
     {
-        static void Main(string[] args)
+        int startIndex = int.Parse(command[1]);
+        int endIndex = int.Parse(command[2]);
+
+
+        if (startIndex < 0)
         {
-            List<string> input = Console.ReadLine()!.Split(' ').ToList();
-            string[] command = Console.ReadLine().Split();
-            while (command[0] != "3:1")
-            {
-                int startIndex = int.Parse(command[1]);
-                int endIndex = int.Parse(command[2]);
-                if (command[0] == "merge")
-                {
-                    if (startIndex < 0) startIndex = 0;
-                    if (endIndex >= input.Count) endIndex = input.Count - 1;
-                    for (int diff = endIndex - startIndex; diff > 0; --diff)
-                    {
-                        input[startIndex] += input[startIndex + 1];
-                        input.RemoveAt(startIndex + 1);
-                    }
-                }
-                if (command[0] == "divide")
-                {
-                    int subParts = int.Parse(command[2]);
-                    int partSize = input[startIndex].Length / endIndex;
-                    int counter = subParts;
-                    int off = 1;
-                    while (counter > 1)
-                    {
-                        counter--;
-                        input.Insert(startIndex + off, new string(input[startIndex].Take(partSize).ToArray()));
-                        off++;
-                        input[startIndex] = new string(input[startIndex].Skip(partSize).ToArray());
-                    }
-                    input.Insert(startIndex + off, input[startIndex]);
-                    input.RemoveAt(startIndex);
-                }
-                command = Console.ReadLine().Split();
-            }
-            Console.WriteLine(string.Join(' ', input));
+            startIndex = 0;
+        }
+
+        if (endIndex >= input.Count)
+        {
+            endIndex = input.Count - 1;
+        }
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            input[startIndex] += input[startIndex + 1];
+            input.RemoveAt(startIndex + 1);
         }
     }
+    else if (command[0] == "divide")
+    {
+        int index = int.Parse(command[1]);
+        int partitions = int.Parse(command[2]);
+        string dividedString = input[index];
+        int subPartitions = dividedString.Length / partitions;
+        int counter = 1;
+
+        for (int i = 1; i < partitions; i++)
+        {
+            string part = new string (dividedString.Take(subPartitions).ToArray());
+
+            input.Insert(index + counter, part);
+            counter++;
+
+            dividedString = new string(dividedString.Skip(subPartitions).ToArray());
+
+        }
+        input.Insert(index + counter, dividedString);
+        input.RemoveAt(index);
+    }
+
+    command = Console.ReadLine().Split().ToArray();
 }
+
+Console.WriteLine(string.Join(" ", input));
