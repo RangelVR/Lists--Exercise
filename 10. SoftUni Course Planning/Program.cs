f34ac493195a5d55d96b41f8b5f9b0ce248da816
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-16 23:06:29 +0200

    Update Program.cs

--- 10. SoftUni Course Planning/Program.cs
+++ 10. SoftUni Course Planning/Program.cs
@@ -1,94 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 
-namespace _10._SoftUni_Course_Planning
+List<string> list = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+string command = Console.ReadLine();
+
+while (command != "course start")
 {
-    class Program
+    string[] arrComm = command.Split(":").ToArray();
+    string lesson = arrComm[1];
+
+    switch (arrComm[0])
     {
-        static void Main(string[] args)
-        {
-            // в Judge дава 88/100.
-            List<string> list = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
-            string[] command = Console.ReadLine().Split(":").ToArray();
-
-            while (command[0] != "course start")
+        case "Add":
+            if (!list.Contains(arrComm[1]))
             {
-                string subComma = command[0];
-                string lesson = command[1];
-                switch (subComma)
+                list.Add(arrComm[1]);
+            }
+            break;
+
+        case "Insert":
+            int index = int.Parse(arrComm[2]);
+
+            if (index >= 0 && index <= list.Count)
+            {
+                if (!list.Contains(lesson))
                 {
-                    case "Add":
-                        if (!list.Contains(lesson))
-                        {
-                            list.Add(lesson);
-                        }
-                        break;
-
-                    case "Insert":
-                        int index = int.Parse(command[2]);
-                        if (!list.Contains(lesson))
-                        {
-                            list.Insert(index, lesson);
-                        }
-                        break;
-
-                    case "Remove":
-                        list.Remove(lesson);
-                        list.Remove(lesson + "-Exercise");
-                        break;
-
-                    case "Swap":
-                        string theOtheLeson = command[2];
-                        if (list.Contains(lesson) && list.Contains(theOtheLeson))
-                        {
-                            for (int i = 0; i < list.Count; i++)
-                            {
-                                if (list[i] == lesson)
-                                {
-                                    list[i] = theOtheLeson;
-                                }
-                                else if (list[i] == theOtheLeson)
-                                {
-                                    list[i] = lesson;
-                                }
-                            }
-                        }
-                        if (list.Contains(lesson + "-Exercise"))
-                        {
-                            list.RemoveAt(list.IndexOf(lesson + "-Exercise"));
-                            list.Insert(list.IndexOf(lesson) + 1, lesson + "-Exercise");
-                            
-                        }
-                        else if (list.Contains(theOtheLeson + "-Exercise"))
-                        {
-                            list.RemoveAt(list.IndexOf(theOtheLeson + "-Exercise"));
-                            list.Insert(list.IndexOf(theOtheLeson) + 1, theOtheLeson + "-Exercise");
-                        }
-                        break;
-
-                    case "Exercise":
-                        if (list.Contains(lesson))
-                        {
-                            list.Insert(list.IndexOf(lesson) + 1, $"{lesson}-Exercise");
-                        }
-                        else if (!list.Contains(lesson))
-                        {
-                            list.Add(lesson);
-                            list.Add($"{lesson}-Exercise");
-                        }
-                        break;
-
+                    list.Insert(index, lesson);
                 }
-                command = Console.ReadLine().Split(":").ToArray();
+            }
+            break;
+
+        case "Remove":
+            if (list.Contains(lesson))
+            {
+                list.Remove(lesson);
+                list.Remove(lesson + "-Exercise");
+            }
+            break;
+
+        case "Exercise":
+            if (!list.Contains(lesson + "-Exercise"))
+            {
+                if (list.Contains(lesson))
+                {
+                    list.Insert(list.IndexOf(lesson) + 1, lesson + "-Exercise");
+                }
+                else
+                {
+                    list.Add(lesson);
+                    list.Add(lesson + "-Exercise");
+                }
+            }
+            
+            break;
+
+        case "Swap":
+            string theOtherlesson = arrComm[2];
+
+            if (list.Contains(lesson) && list.Contains(theOtherlesson))
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] == lesson)
+                    {
+                        list[i] = theOtherlesson;
+                    }
+                    else if (list[i] == theOtherlesson)
+                    {
+                        list[i] = lesson;
+                    }
+                }
             }
 
-            int counter = 1;
-            for (int i = 0; i < list.Count; i++)
+            if (list.Contains(lesson + "-Exercise"))
             {
-                Console.WriteLine($"{counter}.{list[i]}");
-                counter++;
+                list.Remove(lesson + "-Exercise");
+                list.Insert(list.IndexOf(lesson) + 1, lesson + "-Exercise");
             }
-        }
+            if (list.Contains(theOtherlesson + "-Exercise"))
+            {
+                list.Remove(theOtherlesson + "-Exercise");
+                list.Insert(list.IndexOf(theOtherlesson) + 1, theOtherlesson + "-Exercise");
+            }
+            break;
     }
+
+    command = Console.ReadLine();
+}
+
+
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine("{0}.{1}", i + 1, list[i]);
 }
